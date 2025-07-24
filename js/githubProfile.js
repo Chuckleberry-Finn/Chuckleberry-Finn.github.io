a90eaba5195c75704a608ad3eef785abefedaa8d
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-07-24 08:00:54 -0400

    better layout

--- js/githubProfile.js
+++ js/githubProfile.js
@@ -5,9 +5,9 @@ fetch('https://api.github.com/users/Chuckleberry-Finn')
   container.innerHTML = `
     <div style="text-align: center; color: white;">
       <img src="${user.avatar_url}" alt="${user.login}" style="width: 120px; border-radius: 50%;"/>
-      <h2 style="margin: 0.5em 0 0.2em;">${user.name || user.login}</h2>
-      <p>${user.bio || ''}</p>
-      <p>${user.followers} followers • ${user.following} following</p>
+      <h2 style="margin: 0.5em 0 0.2em; font-size: 1.65em;">${user.name || user.login}</h2>
+      <p style="margin: 0 auto; max-width: 280px; text-align: center; font-size: 0.95em; padding: 0.5em 1em;">${user.bio || ''}</p>
+      <p style="margin: 0 auto; max-width: 280px; text-align: center; font-size: 0.75em; padding: 1em 1em;">${user.followers} followers • ${user.following} following</p>
       <a href="${user.html_url}" target="_blank" style="color: #58a6ff;">View on GitHub</a>
     </div>`;
 })
